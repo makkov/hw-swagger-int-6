841dfb5811897521bb03e18c778ba854694184ab
maks <maxim_86nva@mail.ru>
2023-07-29 07:38:39 +0300

    init project

---
+++ src/main/java/com/example/hwswaggerint6/HwSwaggerInt6Application.java
@@ -0,0 +1,13 @@
+package com.example.hwswaggerint6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSwaggerInt6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSwaggerInt6Application.class, args);
+    }
+
+}
